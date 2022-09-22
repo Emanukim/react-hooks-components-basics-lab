a6afda06ccdc1bcda42b77738a6792d203377c20
Emanukim <emmanue.kimweli@student.moringaschool.com>
2022-09-22 14:20:35 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,72 @@
 import React from "react";
+import NavBar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home /> 
+      <About />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+// function NavBar() {
+//   return (
+//     <nav>
+//       <a href="#home">I'm a link!</a>
+//     </nav>
+//   );
+// }
+
+// function Home() {
+//   return (
+//     <div id="home" >
+//       <h1>Home</h1>
+//     </div>
+//   );
+// }
+
+// function About(){
+//   return (
+//     <div id="home">
+//       <h2>About</h2>
+//     </div>
+//   );
+// }
+
+
+
+
+// function NavBar() {
+//   return (
+//     <nav>
+//       <a href="#home">I'm a link!</a>
+//     </nav>
+//   );
+// }
+
+// function Home() {
+//   return (
+//     <div id="home" >
+//       <h1>Home</h1>
+//     </div>
+//   );
+// }
+
+// function About(){
+//   return (
+//     <div id="home">
+//       <h2>About</h2>
+//     </div>
+//   );
+// }
